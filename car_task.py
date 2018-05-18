d1324b560100fc18d8f0b021d7c54eac0c2ff824
Vladimir Kravtsov <vladhc@gmail.com>
2018-05-18 15:04:43 +0200

    Increased state size of the test task by including intermediate substates

--- car_task.py
+++ car_task.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 
 # More simple task for testing the Agent
 class Task():
@@ -7,21 +8,31 @@ class Task():
         env = gym.make('MountainCarContinuous-v0')
         self.env = env
 
-        self.state_size = env.observation_space.shape[0]
+        self.action_repeat = 3
+        self.state_size = env.observation_space.shape[0] * self.action_repeat
         self.action_size = env.action_space.shape[0]
         self.action_low = env.action_space.low
         self.action_high = env.action_space.high
 
     def step(self, action):
-        for _ in range(3):
+        substates = []
+        for _ in range(self.action_repeat):
             next_state, reward, done, _ = self.env.step(action)
+            substates.append(next_state)
             if done:
-                return next_state, reward, done
-        return next_state, reward, done
+                break
+        while len(substates) < self.action_repeat:
+            substates.append(next_state)
+        state = np.concatenate(substates)
+        return state, reward, done
 
     def reset(self):
         """Reset the sim to start a new episode."""
-        state = self.env.reset()
+        next_state = self.env.reset()
+        substates = []
+        while len(substates) < self.action_repeat:
+            substates.append(next_state)
+        state = np.concatenate(substates)
         return state
 
     def close(self):
